fb3af8537f0b1bbdf5758b066d8bd54b672f8a3e
Shawn Bhogal <shawnbhogal@gmail.com>
2020-08-05 19:15:55 -0700

    updatez

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 from flask import Flask 
 from flask import render_template
+from prompts import user_prompt
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', len=len(user_prompt), value=user_prompt)
 
 if __name__ == '__main__':
     app.run(debug=True)
